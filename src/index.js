151e70c5e2809009cd8a6419563bd4ff9ad2534e
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-01-19 11:50:18 +0200

    setup express-hbs and css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
+require('./config/hbsConfig')(app);
 
 app.get('/', (req, res) => {
-    res.send('Hello!')
+    res.render('home')
 });
 
 app.listen(port, () => console.log(`The app is running on port ${port}...`));
